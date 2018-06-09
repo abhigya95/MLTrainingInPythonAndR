2a04bffa6fb86ab747f897ba7fd21c689fe9e5f2
Abhigya Ghimire <abhigya95@users.noreply.github.com>
2018-06-08 21:09:57 -0400

    visualizing polynomial regression

--- Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial_Regression/Local/data_preprocessing_template.R
+++ Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial_Regression/Local/data_preprocessing_template.R
@@ -19,3 +19,56 @@ dataset = dataset[2:3]
 # Feature Scaling
 # training_set = scale(training_set)
 # test_set = scale(test_set)
+
+# Fitting Linear Regression to the dataset
+lin_reg = lm(formula = Salary ~ Level
+             , data = dataset
+             )
+
+# Fitting Polynomial Regression to the dataset
+
+dataset$Level2 = dataset$Level^2 
+dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
+#dataset$Level5 = dataset$Level^5
+
+poly_reg = lm(formula = Salary ~ .
+              , data = dataset
+              )
+
+# Visualing the linear regrssion results
+# install.packages('ggplot2')
+
+library(ggplot2)
+
+ggplot() +
+    geom_point(aes(x = dataset$Level, y = dataset$Salary)
+            , color = 'red') +
+    geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)),
+              color = 'blue') +
+    ggtitle('Truth or Bluff? (Linear Regression)') +
+    xlab('Level') +
+    ylab('Salary')
+
+# Visualizing the Polynomial Regression results
+
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary)
+             , color = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)),
+            color = 'blue') +
+  ggtitle('Truth or Bluff? (Linear Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+
+# Predicting a new result with Linear Regression
+y_pred = predict(lin_reg, data.frame(Level = 6.5))
+
+# Prediting a new result with Polynomial Regression
+y_pred = predict(poly_reg, data.frame(Level = 6.5, 
+                                      Level2 = 6.5^2, 
+                                      Level3 = 6.5^3,
+                                      Level4 = 6.5^4
+                                      ))
+                                      #Level5 = 6.5^5))
